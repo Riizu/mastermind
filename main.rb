824353443f5e0aed4392a600674bcf1b9cf03faa
Ryan Batty <rgbatty@outlook.com>
2016-03-24 14:54:18 -0600

    Fixed main implementation

--- main.rb
+++ main.rb
@@ -1,52 +1,49 @@
 require_relative 'game'
 require_relative 'highscore'
 
-class Main
-  $state = "menu"
-  @@game = Game.new
 
-  def main
-    running = true
+$state = "menu"
 
-    while running
-      case $state
-        when "menu"
-          print_menu
-          getInput
-        when "game_start"
-          @@game.start
-        when "highscore"
-          #puts "In highscore"
-          getInput
-        when "exit"
-          running = false
-      end
-    end
-  end
 
-  def print_menu
-    puts "----------------------------------"
-    puts "|          welcome to            |"
-    puts "|          MASTERMIND            |"
-    puts "|  (P)lay (H)igh Scores (E)xit   |"
-    puts "----------------------------------"
-  end
-
-  def getInput
-    print "Please enter a command:"
-    input = gets.chomp.upcase
-
-    case input
-      when "P"
-        $state = "game_start"
-      when "H"
-        $state = "highscore"
-      when "E"
-        $state = "exit"
-    end
-  end
-
+def print_menu
+  puts "-------------------------------------------------"
+  puts "|                 welcome to                    |"
+  puts "|                 MASTERMIND                    |"
+  puts "|  (P)lay (H)igh Scores (I)nstructions (E)xit   |"
+  puts "-------------------------------------------------"
 end
 
-main = Main.new
-main.main
+def getInput
+  print "Please enter a command:"
+  input = gets.chomp.upcase
+
+  case input
+  when "P"
+    $state = "game_start"
+  when "H"
+    $state = "highscore"
+  when "I"
+    $state = "instructions"
+  when "E"
+    $state = "exit"
+  end
+end
+
+running = true
+
+while running
+  case $state
+  when "menu"
+    print_menu
+    getInput
+  when "game_start"
+    game = Game.new
+    game.start
+  when "highscore"
+    getInput
+  when "instructions"
+    getInput
+  when "exit"
+    running = false
+  end
+end
